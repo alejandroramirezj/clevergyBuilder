240c7d1f02c8bb4887b6d4d1f2d671329b098ca2
Alejandro Ramirez <48692145+alejandroramirezj@users.noreply.github.com>
2025-05-30 21:34:55 +0200

    fix: ajuste de la ruta base para desarrollo local

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/clevergyBuilder/',
+  base: mode === 'production' ? '/clevergyBuilder/' : '/',
   plugins: [
     react(),
     mode === 'development' &&
